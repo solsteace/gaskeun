532705dba715b693b8323b69b653ac479cae23b6
Atha Ahsan Xavier Haris <atha.ahsan.xavier.haris.com>
2024-04-26 06:33:58 +0700

    fix /booking button enable or disable

--- public/js/booking.js
+++ public/js/booking.js
@@ -2,5 +2,5 @@ const checkbox = document.getElementById("flexCheckDefault");
 const submitButton = document.getElementById("gas-button");
 
 checkbox.addEventListener("change", function () {
-    submitButton.disabled = !(this.checked && inputFile.files.length > 0);
+    submitButton.disabled = !this.checked;
 });
