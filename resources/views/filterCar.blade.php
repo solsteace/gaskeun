1318ded536d8349ff26ab48537ac6cdbb57cac73
stefhan235 <stefhansalim12@gmail.com>
2024-04-27 17:25:50 +0700

    fix filter jumlah penumpang

--- resources/views/filterCar.blade.php
+++ resources/views/filterCar.blade.php
@@ -122,7 +122,7 @@
                 <option value="5">5</option>
                 <option value="6">6</option>
                 <option value="7">7</option>
-                <option value="8" selected>8</option>>
+                <option value="8">8</option>>
               </select>
             </div>
           </div>
