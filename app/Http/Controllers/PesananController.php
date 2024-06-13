0313b478c416767273365079e069133cb77892a7
Allen <valternatean13@gmail.com>
2024-06-13 16:19:02 +0700

    fix: forgotten `DB` import

--- app/Http/Controllers/PesananController.php
+++ app/Http/Controllers/PesananController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Pesanan;
 use App\Models\Mobil;
